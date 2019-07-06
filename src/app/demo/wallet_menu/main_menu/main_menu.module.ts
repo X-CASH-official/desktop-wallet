98966ea2c14eb31232c9b0cfc96f4f8212fb9d42
zachhildreth <zachariahildreth@gmail.com>
2019-07-06 13:12:43 -0700

    Added xcashd progress bar loader
    Added xcashd progress bar loader

--- src/app/demo/wallet_menu/main_menu/main_menu.module.ts
+++ src/app/demo/wallet_menu/main_menu/main_menu.module.ts
@@ -4,14 +4,14 @@ import { CommonModule } from '@angular/common';
 import { main_menuRoutingModule } from './main_menu-routing.module';
 import { main_menuComponent } from './main_menu.component';
 import {SharedModule} from '../../../theme/shared/shared.module';
-import {NgbCarouselModule} from '@ng-bootstrap/ng-bootstrap';
+import {NgbProgressbarModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
     CommonModule,
     main_menuRoutingModule,
     SharedModule,
-    NgbCarouselModule
+    NgbProgressbarModule
   ],
   declarations: [main_menuComponent]
 })
