856bef73efb0702bab689be5c9fc505efaf56c4f
zachhildreth <zachariahildreth@gmail.com>
2019-07-07 15:29:42 -0700

    Added view public address
    Added view public address.

--- src/app/demo/basic_wallet_functions/view_public_address/view_public_address.component.ts
+++ src/app/demo/basic_wallet_functions/view_public_address/view_public_address.component.ts
@@ -7,7 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class view_public_addressComponent implements OnInit {
 
+  // Variables
+  data:string;
+  data2:string;
+  public_address_settings:boolean = false;
+  public_address:string = "";
+
   constructor() { }
+
+  copyMessage(text: string){
+    let selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = text;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);    
+  }
+
+  timer_data(data:string)
+  {
+    this.data2 = data;
+    this.public_address_settings = true;
+    setTimeout(() => this.public_address_settings = false, 5000);
+  }
 
   ngOnInit()
   {
