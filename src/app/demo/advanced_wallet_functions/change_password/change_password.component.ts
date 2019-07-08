8c6119f87a0f8db2d8cb2b9e981a45271e94ec83
zachhildreth <zachariahildreth@gmail.com>
2019-07-07 20:54:34 -0700

    Added change_password
    Added change_password.

--- src/app/demo/advanced_wallet_functions/change_password/change_password.component.ts
+++ src/app/demo/advanced_wallet_functions/change_password/change_password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {variables_and_functions_service} from '../../../services/variables_and_functions.service';
 
 @Component({
   selector: 'app-change_password',
@@ -7,10 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class change_passwordComponent implements OnInit {
 
-  constructor() { }
+  // Variables
+  data:any = {
+    current_password: "",
+    new_password: "",
+    confirm_password: ""
+  };
+  settings:boolean = false;
+
+  constructor(private variables_and_functions_service: variables_and_functions_service) { }
 
   ngOnInit()
   {
-  
+    this.reset_data();
+  } 
+
+  reset_data()
+  {
+    this.data = {
+      current_password: "",
+      new_password: "",
+      confirm_password: ""
+    };  
+  }
+
+  timer_data()
+  {
+    this.settings = true;
+    setTimeout(() => this.settings = false, 5000);
+  }
+
+  change_password()
+  {
+    if (this.variables_and_functions_service.text_settings.test(this.data.current_password) && this.variables_and_functions_service.text_settings.test(this.data.new_password) && this.variables_and_functions_service.text_settings.test(this.data.confirm_password))
+    {
+      this.timer_data();
+      return true;
+    }
+    return false;
   }
 }
