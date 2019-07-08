78305729dce52ddbded4daef39969a3ba52ce92f
zachhildreth <zachariahildreth@gmail.com>
2019-07-07 21:02:13 -0700

    Added rescan_blockchain
    Added rescan_blockchain.

--- src/app/demo/advanced_wallet_functions/rescan_blockchain/rescan_blockchain.module.ts
+++ src/app/demo/advanced_wallet_functions/rescan_blockchain/rescan_blockchain.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { rescan_blockchainRoutingModule } from './rescan_blockchain-routing.module';
 import { rescan_blockchainComponent } from './rescan_blockchain.component';
 import {SharedModule} from '../../../theme/shared/shared.module';
+import {NgbProgressbarModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
     CommonModule,
     rescan_blockchainRoutingModule,
-    SharedModule
+    SharedModule,
+    NgbProgressbarModule
   ],
   declarations: [rescan_blockchainComponent]
 })
