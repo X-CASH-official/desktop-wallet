2dac8c824b976864c856866b451b8ef06af12c1e
zachhildreth <zachariahildreth@gmail.com>
2019-07-10 08:10:39 -0700

    Added code to verify_tx_private_key
    Added code to verify_tx_private_key

--- src/app/demo/advanced_wallet_functions/verify_tx_private_key/verify_tx_private_key.component.ts
+++ src/app/demo/advanced_wallet_functions/verify_tx_private_key/verify_tx_private_key.component.ts
@@ -9,12 +9,18 @@ import {variables_and_functions_service} from '../../../services/variables_and_f
 export class verify_tx_private_keyComponent implements OnInit {
 
   // Variables
+  error_title:string = "";
+  error_message:string = "";
+  error:any = {
+    error_settings: false
+  }  
   data:any = {
     transaction_hash: "",
     tx_private_key: "",
     public_address: ""
   };
   settings:boolean = false;
+  amount:string = "";
 
   constructor(private variables_and_functions_service: variables_and_functions_service) { }
 
@@ -32,14 +38,28 @@ export class verify_tx_private_keyComponent implements OnInit {
     };  
   }
 
-  verify_tx_private_key()
-  { 
+  async verify_tx_private_key()
+  {
     if (this.variables_and_functions_service.private_key.test(this.data.transaction_hash) && this.variables_and_functions_service.private_key.test(this.data.tx_private_key) && this.variables_and_functions_service.xcash_address.test(this.data.public_address))
     {
-      this.settings = true;
-      return true;
+      // Constants
+      const data:string = `{"jsonrpc":"2.0","id":"0","method":"check_tx_key","params":{"txid":"${this.data.transaction_hash}","tx_key":"${this.data.tx_private_key}","address":"${this.data.public_address}"}}`;
+      
+      // Variables
+      let data2:any;
+
+      data2 = await this.variables_and_functions_service.send_post_request(data, this.error);
+      if (this.error.error_settings === false)
+      {
+        this.settings = true;
+        this.amount = data2.result.received;
+      }
+      else
+      {
+        this.error_title = "Verify Transaction Private Key";
+        this.error_message = data2.error.message;
+        setTimeout(() => document.getElementById("error").click(), 1000);        
+      }
     }
-    this.settings = false;
-    return false;
   }
 }
