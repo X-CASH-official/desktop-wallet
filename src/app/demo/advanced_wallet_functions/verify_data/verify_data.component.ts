b2ca847169a4adf989d67279b131e8979951e3d5
zachhildreth <zachariahildreth@gmail.com>
2019-07-10 09:23:31 -0700

    Added code to verify_data
    Added code to verify_data

--- src/app/demo/advanced_wallet_functions/verify_data/verify_data.component.ts
+++ src/app/demo/advanced_wallet_functions/verify_data/verify_data.component.ts
@@ -9,13 +9,18 @@ import {variables_and_functions_service} from '../../../services/variables_and_f
 export class verify_dataComponent implements OnInit {
 
   // Variables
+  error_title:string = "";
+  error_message:string = "";
+  error:any = {
+    error_settings: false
+  }  
   data:any = {
     signature: "",
     public_address: "",
     message: ""
   };
   signature_settings:boolean = false;
-  settings:boolean = false;
+  settings:string = "";
 
   constructor(private variables_and_functions_service: variables_and_functions_service) { }
 
@@ -33,15 +38,28 @@ export class verify_dataComponent implements OnInit {
     };  
   }
 
-  verify_signature()
-  { 
-    this.signature_settings = true;
+  async verify_signature()
+  {
     if (this.variables_and_functions_service.signature.test(this.data.signature) && this.variables_and_functions_service.xcash_address.test(this.data.public_address))
     {
-      this.settings = true;
-      return true;
+      // Constants
+      const data:string = this.data.message == "" ? `{"jsonrpc":"2.0","id":"0","method":"verify","params":{"address":"${this.data.public_address}","signature":"${this.data.signature}"}}` : `{"jsonrpc":"2.0","id":"0","method":"verify","params":{"address":"${this.data.public_address}","signature":"${this.data.signature}","data":"${this.data.message}"}}`;
+      
+      // Variables
+      let data2:any;
+
+      data2 = await this.variables_and_functions_service.send_post_request(data, this.error);
+      if (this.error.error_settings === false)
+      {
+        this.signature_settings = true;
+        this.settings = data2.result.good ? "The signature is valid" : "The signature is invalid";
+      }
+      else
+      {
+        this.error_title = "Verify data";
+        this.error_message = data2.error.message;
+        setTimeout(() => document.getElementById("error").click(), 1000);        
+      }
     }
-    this.settings = false;
-    return false;
   }
 }
