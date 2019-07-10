00dc07415935e24673c18e13eb85abd5f560c344
zachhildreth <zachariahildreth@gmail.com>
2019-07-09 20:25:21 -0700

    Added code to send
    Added code to send

--- src/app/demo/basic_wallet_functions/send/send.component.ts
+++ src/app/demo/basic_wallet_functions/send/send.component.ts
@@ -9,9 +9,17 @@ import {variables_and_functions_service} from '../../../services/variables_and_f
 export class sendComponent implements OnInit {
 
   // Variables
+  error_title:string = "";
+  error_message:string = "";
+  error:any = {
+    error_settings: false
+  }  
   data:any = {
     public_address: "",
+    transaction_hash: [],
+    transaction_key: [],
     amount: "",
+    fee: "",
     payment_id: "",
     public_transaction_settings: false
   };
@@ -43,16 +51,106 @@ export class sendComponent implements OnInit {
   {
     this.data = {
       public_address: "",
+      transaction_hash: [],
+      transaction_key: [],
       amount: "",
+      fee: "",
       payment_id: "",
       public_transaction_settings: false
     };  
   }
 
-  send()
+  async get_transaction_fee()
   {
     // verify that the data is correct for sending the transaction
-    return this.variables_and_functions_service.xcash_address.test(this.data.public_address) && this.variables_and_functions_service.xcash_amount.test(this.data.amount) && this.variables_and_functions_service.payment_id.test(this.data.payment_id);
+    if (this.variables_and_functions_service.xcash_address.test(this.data.public_address) && this.variables_and_functions_service.xcash_amount.test(this.data.amount) && this.variables_and_functions_service.payment_id.test(this.data.payment_id))
+    {
+       // Variables
+       let data:string;
+       let data2:any;     
+      if (this.data.amount === "FULL_BALNCE")
+      {
+        if (!this.data.public_transaction_settings)
+        {
+          data = this.data.payment_id == "" ? '{"jsonrpc":"2.0","id":"0","method":"sweep_all","params":{"destinations":[{"amount":' + this.variables_and_functions_service.xcash_amount_settings(this.data.amount,0) + ',"address":"' + this.data.public_address + '"}],"priority":0,"ring_size":21,"get_tx_keys": true, "do_not_relay":true}}' : '{"jsonrpc":"2.0","id":"0","method":"transfer_split","params":{"destinations":[{"amount":' + this.variables_and_functions_service.xcash_amount_settings(this.data.amount1000000000000,0) + ',"address":"' + this.data.public_address + '"}],"payment_id":"' + this.data.payment_id + '"priority":0,"ring_size":21,"get_tx_keys": true, "do_not_relay":true}}';
+        }
+        else
+        {
+          data = this.data.payment_id == "" ? '{"jsonrpc":"2.0","id":"0","method":"sweep_all","params":{"destinations":[{"amount":' + this.variables_and_functions_service.xcash_amount_settings(this.data.amount,0) + ',"address":"' + this.data.public_address + '"}],"priority":0,"ring_size":21,"get_tx_keys": true, "do_not_relay":true}}' : '{"jsonrpc":"2.0","id":"0","method":"transfer_split","params":{"destinations":[{"amount":' + this.variables_and_functions_service.xcash_amount_settings(this.data.amount1000000000000,0) + ',"address":"' + this.data.public_address + '"}],"tx_privacy_settings":"public", "payment_id":"' + this.data.payment_id + '"priority":0,"ring_size":21,"get_tx_keys": true, "do_not_relay":true}}';
+        }
+      }
+      else
+      {
+        if (!this.data.public_transaction_settings)
+        {
+          data = this.data.payment_id == "" ? '{"jsonrpc":"2.0","id":"0","method":"transfer_split","params":{"destinations":[{"amount":' + this.variables_and_functions_service.xcash_amount_settings(this.data.amount,0) + ',"address":"' + this.data.public_address + '"}],"priority":0,"ring_size":21,"get_tx_keys": true, "do_not_relay":true}' : '{"jsonrpc":"2.0","id":"0","method":"transfer_split","params":{"destinations":[{"amount":' + this.variables_and_functions_service.xcash_amount_settings(this.data.amount1000000000000,0) + ',"address":"' + this.data.public_address + '"}],"payment_id":"' + this.data.payment_id + '"priority":0,"ring_size":21,"get_tx_keys": true, "do_not_relay":true}}';
+        }
+        else
+        {
+          data = this.data.payment_id == "" ? '{"jsonrpc":"2.0","id":"0","method":"transfer_split","params":{"destinations":[{"amount":' + this.variables_and_functions_service.xcash_amount_settings(this.data.amount,0) + ',"address":"' + this.data.public_address + '"}],"priority":0,"ring_size":21,"get_tx_keys": true, "do_not_relay":true}}' : '{"jsonrpc":"2.0","id":"0","method":"transfer_split","params":{"destinations":[{"amount":' + this.variables_and_functions_service.xcash_amount_settings(this.data.amount1000000000000,0) + ',"address":"' + this.data.public_address + '"}],"tx_privacy_settings":"public", "payment_id":"' + this.data.payment_id + '"priority":0,"ring_size":21,"get_tx_keys": true, "do_not_relay":true}}';
+        }
+      }
+
+      data2 = await this.variables_and_functions_service.send_post_request(data, this.error);
+      if (this.error.error_settings === false)
+      {
+        this.data.fee = this.variables_and_functions_service.xcash_amount_settings(data2.result.fee_list[0],1);
+        setTimeout(() => document.getElementById("send_transaction_one_data").click(), 1000); 
+      }
+      else
+      {
+        this.error_title = "Send";
+        this.error_message = data2.error.message;
+        setTimeout(() => document.getElementById("error").click(), 1000);        
+      } 
+    }
+  }
+
+  async send()
+  {
+    // verify that the data is correct for sending the transaction
+    if (this.variables_and_functions_service.xcash_address.test(this.data.public_address) && this.variables_and_functions_service.xcash_amount.test(this.data.amount) && this.variables_and_functions_service.payment_id.test(this.data.payment_id))
+    {
+       // Variables
+       let data:string;
+       let data2:any;     
+      if (this.data.amount === "FULL_BALNCE")
+      {
+        if (!this.data.public_transaction_settings)
+        {
+          data = this.data.payment_id == "" ? '{"jsonrpc":"2.0","id":"0","method":"sweep_all","params":{"destinations":[{"amount":' + this.variables_and_functions_service.xcash_amount_settings(this.data.amount,0) + ',"address":"' + this.data.public_address + '"}],"priority":0,"ring_size":21,"get_tx_keys": true, "do_not_relay":true}}' : '{"jsonrpc":"2.0","id":"0","method":"transfer_split","params":{"destinations":[{"amount":' + this.variables_and_functions_service.xcash_amount_settings(this.data.amount1000000000000,0) + ',"address":"' + this.data.public_address + '"}],"payment_id":"' + this.data.payment_id + '"priority":0,"ring_size":21,"get_tx_keys": true, "do_not_relay":true}}';
+        }
+        else
+        {
+          data = this.data.payment_id == "" ? '{"jsonrpc":"2.0","id":"0","method":"sweep_all","params":{"destinations":[{"amount":' + this.variables_and_functions_service.xcash_amount_settings(this.data.amount,0) + ',"address":"' + this.data.public_address + '"}],"priority":0,"ring_size":21,"get_tx_keys": true, "do_not_relay":true}}' : '{"jsonrpc":"2.0","id":"0","method":"transfer_split","params":{"destinations":[{"amount":' + this.variables_and_functions_service.xcash_amount_settings(this.data.amount1000000000000,0) + ',"address":"' + this.data.public_address + '"}],"tx_privacy_settings":"public", "payment_id":"' + this.data.payment_id + '"priority":0,"ring_size":21,"get_tx_keys": true, "do_not_relay":true}}';
+        }
+      }
+      else
+      {
+        if (!this.data.public_transaction_settings)
+        {
+          data = this.data.payment_id == "" ? '{"jsonrpc":"2.0","id":"0","method":"transfer_split","params":{"destinations":[{"amount":' + this.variables_and_functions_service.xcash_amount_settings(this.data.amount,0) + ',"address":"' + this.data.public_address + '"}],"priority":0,"ring_size":21,"get_tx_keys": true, "do_not_relay":true}' : '{"jsonrpc":"2.0","id":"0","method":"transfer_split","params":{"destinations":[{"amount":' + this.variables_and_functions_service.xcash_amount_settings(this.data.amount1000000000000,0) + ',"address":"' + this.data.public_address + '"}],"payment_id":"' + this.data.payment_id + '"priority":0,"ring_size":21,"get_tx_keys": true, "do_not_relay":true}}';
+        }
+        else
+        {
+          data = this.data.payment_id == "" ? '{"jsonrpc":"2.0","id":"0","method":"transfer_split","params":{"destinations":[{"amount":' + this.variables_and_functions_service.xcash_amount_settings(this.data.amount,0) + ',"address":"' + this.data.public_address + '"}],"priority":0,"ring_size":21,"get_tx_keys": true, "do_not_relay":true}}' : '{"jsonrpc":"2.0","id":"0","method":"transfer_split","params":{"destinations":[{"amount":' + this.variables_and_functions_service.xcash_amount_settings(this.data.amount1000000000000,0) + ',"address":"' + this.data.public_address + '"}],"tx_privacy_settings":"public", "payment_id":"' + this.data.payment_id + '"priority":0,"ring_size":21,"get_tx_keys": true, "do_not_relay":true}}';
+        }
+      }
+
+      data2 = await this.variables_and_functions_service.send_post_request(data, this.error);
+      if (this.error.error_settings === false)
+      {
+        this.data.transaction_hash = data2.result.tx_hash_list;
+        this.data.transaction_key = data2.result.tx_key_list;
+        setTimeout(() => document.getElementById("send_transaction_two_data").click(), 1000); 
+      }
+      else
+      {
+        this.error_title = "Send";
+        this.error_message = data2.error.message;
+        setTimeout(() => document.getElementById("error").click(), 1000);        
+      } 
+    }
   }
 
   verify_data(item:string, data:string)
