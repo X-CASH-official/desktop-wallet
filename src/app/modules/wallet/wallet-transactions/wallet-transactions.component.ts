8290e5ece654290639fb917c9ab93fca61a5def0
mtardy <mahe5397@hotmail.fr>
2019-11-01 11:02:13 +0100

    Fix conflict between sort and expand in table https://github.com/angular/components/issues/15380#issuecomment-473246803

--- src/app/modules/wallet/wallet-transactions/wallet-transactions.component.ts
+++ src/app/modules/wallet/wallet-transactions/wallet-transactions.component.ts
@@ -8,9 +8,9 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
   styleUrls: ['./wallet-transactions.component.scss'],
   animations: [
     trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('collapsed, void', style({height: '0px', minHeight: '0'})),
       state('expanded', style({height: '*'})),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+      transition('expanded <=> collapsed, void => expanded', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
   ],
 })
